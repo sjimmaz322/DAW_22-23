adb3ba6441e27d3dad509685b5af35111ebda617
SamueljmDevs <samueljmdevs@gmail.com>
2022-09-22 19:16:22 +0200

    Mejorado el text block

--- tema2SamuelJimenez/src/main/java/ejercicios/Ej14.java
+++ tema2SamuelJimenez/src/main/java/ejercicios/Ej14.java
@@ -17,30 +17,26 @@ public class Ej14 {
         //Declaramos e inicializamos un objeto de la clase Scanner
         Scanner sc = new Scanner(System.in);
         //Declaramos las variables que usaremos
-        int horas, minutos, segundosRestantes;
+        int horas, minutos, segundos;
 
         //Mensaje para informar de lo que se requiere que se introduzca
         System.out.println("Introduzca los segundos que quiera");
-        
+
         //Declaramos e inicializamos con lo introducido por Scanner la variable segundos.      
-        int segundos = sc.nextInt();
-        
+        int tiempo = sc.nextInt();
+
         //Inicializamos con los cálculos las variables horas, minutos y segundos restantes
-        horas = segundos/3600; //Para calcular las horas hacemos el cálculo de los segundos introducimos partido los segundos en una hora
-        minutos = (segundos%3600)/60;// Para calcular los minutos cogemos el resto del cálculo anterior y lo dividimos por los minutos en una hora
-        segundosRestantes = (segundos%60); //Los segundos restantes sería el resto de los segundo entre los segundos en un minuto
-        
+        horas = tiempo / 3600; //Para calcular las horas hacemos el cálculo de los segundos introducimos partido los segundos en una hora
+        minutos = (tiempo % 3600) / 60;// Para calcular los minutos cogemos el resto del cálculo anterior y lo dividimos por los minutos en una hora
+        segundos = (tiempo % 60); //Los segundos restantes sería el resto de los segundo entre los segundos en un minuto
+
         //Mostramos por consola el resultado.
-        System.out.printf(segundos+" son:"
-         + "\nHoras: "+horas
-        +"\nMinutos: "+minutos
-        +"\nSegundos: "+ segundosRestantes);
-        
-//        //Mostramos por ventana los resultados.
-//        JOptionPane.showMessageDialog(null, (segundos+" son:"
-//         + "\nHoras: "+horas
-//        +"\nMinutos: "+minutos
-//        +"\nSegundos: "+ segundosRestantes));
+        System.out.println("""
+                           %d segundos son:
+                           %d horas.
+                           %d minutos.
+                           %d segundos.
+                           """.formatted(tiempo, horas, minutos, segundos));
 
     }
 
