5471c4719d35621ff67bf23af52852d1af6f5bb8
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-10 14:41:54 +0200

    Añadido enunciado

--- tema3SamuelJimenez/src/main/java/estructurasderepeticion/Ej05.java
+++ tema3SamuelJimenez/src/main/java/estructurasderepeticion/Ej05.java
@@ -4,14 +4,19 @@
  */
 package estructurasderepeticion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sjimmaz322 <sjimmaz322@g.educaand.es>
  */
 public class Ej05 {
 
-    /**
-     * @param args the command line arguments
+    /*
+    Implementa y completa el siguiente programa.
+    Ten en cuenta que cuando se entre en cada una de las opciones (1,2 o 3) el programa realice su función.
+    Esto es, si se selecciona la opción 1, el programa debe entrar en la parte de calcular el área de un cuadrado
+    y debe solicitar los dos lados y mostrar el resultado. Así con el resto de opciones.
      */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
