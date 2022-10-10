6e15ea4055b5de240fffb323856e01797380d6b4
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-10 14:35:50 +0200

    Creado y completado el ejercicio 3

---
+++ tema3SamuelJimenez/src/main/java/estructurasderepeticion/Ej03.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estructurasderepeticion;
+
+/**
+ *
+ * @author sjimmaz322 <sjimmaz322@g.educaand.es>
+ */
+public class Ej03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+//        boolean unaCondicion = true;
+//        resultado = unaCondicion ? valor1 : valor2;
+//        En este caso devolvería un String que contendría valor1.
+
+//        int x = 10;
+//        int y = (x > 9) ? 100 : 200;
+//        Aquí devolvería 100.
+
+//        int publico = 19500;
+//        int vendidas = 19000;
+//        int aforo = 2000;
+//        String status = (publico < aforo) ? "Cabe mas gente" : (vendidas < aforo) ? "Aun no hemos vendido todo" : "Esta todo vendido";
+//        Devolvería "Aún no hemos vendido todo"
+    }
+
+}
