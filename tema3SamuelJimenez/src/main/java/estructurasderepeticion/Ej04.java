9a6bb88843854fdad42d4a74f17bc1427bee2324
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-10 14:37:55 +0200

    Creada clase Ej04 y realizado el ejercicio

---
+++ tema3SamuelJimenez/src/main/java/estructurasderepeticion/Ej04.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estructurasderepeticion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sjimmaz322 <sjimmaz322@g.educaand.es>
+ */
+public class Ej04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce tu edad:");
+
+        int edad = scanner.nextInt();
+
+        String resultado = (edad >= 18) ? "Es mayor de edad" : "No es mayor de edad";
+
+        System.out.println(resultado);
+    }
+
+}
