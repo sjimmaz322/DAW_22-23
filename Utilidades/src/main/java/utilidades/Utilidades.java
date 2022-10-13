c2e122a9e5d2043e0cb0b53c4ebaf4de40f7253a
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-13 10:30:38 +0200

    Creado proyecto y clase para guardar mis utilidades

---
+++ Utilidades/src/main/java/utilidades/Utilidades.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package utilidades;
+
+/**
+ *
+ * @author sjimmaz322 <sjimmaz322@g.educaand.es>
+ */
+public class Utilidades {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
