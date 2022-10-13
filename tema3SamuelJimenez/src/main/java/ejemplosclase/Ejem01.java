fe498540d92e21dd270c3907a7ab0ab11b78f3d5
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-13 10:33:32 +0200

    Creado ejemplo 1

---
+++ tema3SamuelJimenez/src/main/java/ejemplosclase/Ejem01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplosclase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sjimmaz322 <sjimmaz322@g.educaand.es>
+ */
+public class Ejem01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuál es tu nombre?");
+        String nombre = sc.nextLine();
+
+    }
+
+}
