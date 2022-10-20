5de17e9e1f3d6e2da0d2507be924fc2aec34ce74
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-20 12:36:17 +0200

    CD

--- MisUtilidades/src/main/java/utilidadespropias/MisUtilidades.java
+++ MisUtilidades/src/main/java/utilidadespropias/MisUtilidades.java
@@ -4,8 +4,6 @@ package utilidadespropias;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-
 import java.util.Random;
 import javax.swing.JOptionPane;
 
