5cfec8a41ce26bbf42bd95850b82eec6137159cb
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-10-21 10:40:38 +0200

    Añadido otro for del 0 al 20 de dos en dos

--- tema3SamuelJimenez/src/main/java/ejemplosclase/EjemploFor.java
+++ tema3SamuelJimenez/src/main/java/ejemplosclase/EjemploFor.java
@@ -21,23 +21,46 @@ public class EjemploFor {
             System.out.println(i);
         }
 
+        System.out.println(" ");
+        System.out.println("Fin del primer bucle");
+        System.out.println(" ");
+
+        //Del 0 al 20 de 2 en 2
+        for (int i = 0; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println(" ");
+        System.out.println("Fin del segundo bucle");
+        System.out.println(" ");
+
         //Del 20 al 0
         for (int i = 20; i >= 0; i--) {
             System.out.println(i);
         }
 
+        System.out.println(" ");
+        System.out.println("Fin del tercer bucle");
+        System.out.println(" ");
+
         //Del 50 al 70 de 3 en 3
         for (int i = 50; i < 70; i += 3) {
             System.out.println(i);
         }
+        System.out.println(" ");
+        System.out.println("Fin del cuarto bucle");
+        System.out.println(" ");
 
         //Pedimos un número entre 100 y 200
         Scanner sc = new Scanner(System.in);
-        
+
         for (int i = 0; i < 100 || i > 200; i = sc.nextInt()) {
             System.out.println("Introduce un número entre 100 y 200");
         }
 
+        System.out.println(" ");
+        System.out.println("Fin del último bucle");
+        System.out.println(" ");
     }
 
 }
