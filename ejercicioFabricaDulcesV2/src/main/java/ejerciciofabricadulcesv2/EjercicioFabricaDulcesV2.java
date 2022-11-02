b1ac0214e3502661c5eca2d0e65e719aa00ca736
SamueljmDevs <samueljmdevs@gmail.com>
2022-11-02 21:04:01 +0100

    Modularizado ejercicio

--- ejercicioFabricaDulcesV2/src/main/java/ejerciciofabricadulcesv2/EjercicioFabricaDulcesV2.java
+++ ejercicioFabricaDulcesV2/src/main/java/ejerciciofabricadulcesv2/EjercicioFabricaDulcesV2.java
@@ -32,16 +32,16 @@ public class EjercicioFabricaDulcesV2 {
     private static final double MAXIMO_COSTE = 1.0;
     private static final String CODIGO_COMPROBAR = "Comprobar";
     private static final String CODIGO_SALIDA = "Salir";
+    private static String code = "";
 
     public static void main(String[] args) {
 
-       
         comprobaCodigoAccion(CODIGO_COMPROBAR, CODIGO_SALIDA);
 
     }
 
     private static void comprobaCodigoAccion(String cod1, String cod2) {
-        String code;
+
         String menu = """
                Introduza que desea hacer
                Comprobar - Para comprobar la viabilidad de los productos.
@@ -74,9 +74,6 @@ public class EjercicioFabricaDulcesV2 {
     }
 
     private static void comprobaCodigoDulce(String menu, String cod1, String cod2, String cod3, String cod4, String cod5) {
-        String code = "", nombreProducto = "";
-
-        double costeMateriaPrimaUnidad = 0, costeManoObra = 0, precioUnidad = 0;
 
         do {
             code = Utilidades.pedirString(menu).toUpperCase();
@@ -85,52 +82,43 @@ public class EjercicioFabricaDulcesV2 {
                 || code.equalsIgnoreCase(cod3)
                 || code.equalsIgnoreCase(cod4)
                 || code.equalsIgnoreCase(cod5)));
+        calcularViabilidad(code);
+    }
 
-        switch (code) {
-            case "M1","M2","T1","T2","P1":
+    private static void calcularViabilidad(String code) {
+        String nombreProducto = "";
 
-                costeMateriaPrimaUnidad = Utilidades.doubleVentanaRangoIncluyente("Introduza el coste de la materia prima para la fabricación de una unidad de producto",
-                        MAXIMO_COSTE, MINIMO_COSTE);
+        double costeMateriaPrimaUnidad = 0, costeManoObra = 0, precioUnidad = 0;
+        costeMateriaPrimaUnidad = Utilidades.doubleVentanaRangoIncluyente("Introduza el coste de la materia prima para la fabricación de una unidad de producto",
+                MAXIMO_COSTE, MINIMO_COSTE);
 
-                costeManoObra = Utilidades.doubleVentanaRangoIncluyente("Introduzca el coste de mano de obra de la fabricación de una unidad de producto",
-                        MANO_OBRA_UNIDAD_MAXIMO, MANO_OBRA_UNIDAD_MINIMO);
+        costeManoObra = Utilidades.doubleVentanaRangoIncluyente("Introduzca el coste de mano de obra de la fabricación de una unidad de producto",
+                MANO_OBRA_UNIDAD_MAXIMO, MANO_OBRA_UNIDAD_MINIMO);
+        double costeProduccionUnidad = calculoPrecioProduccion(costeMateriaPrimaUnidad, costeManoObra);
 
-                double costeProduccionUnidad = costeMateriaPrimaUnidad + costeManoObra; //Calculamos el coste de producir una unidad
+        double precioVentaUnidad = calculoPrecioUnidad(code, costeProduccionUnidad);
 
-                final double PRECIO_UNIDAD_M1_M2_P1 = costeProduccionUnidad + (costeProduccionUnidad / 2);
-                final double PRECIO_UNIDAD_T1_T2 = (costeProduccionUnidad + (costeProduccionUnidad * 65) / 100);
+        switch (code) {//Dependiendo del código guardamos el nombre del producto en una variable para mostrarlo luego
+            case "M1" -> {
+                nombreProducto = "Mantecados de Limón";
+            }
+            case "M2" -> {
+                nombreProducto = "Mazapán";
+            }
+            case "P1" -> {
+                nombreProducto = "Polvorón";
+            }
+            case "T1" -> {
+                nombreProducto = "Turrón de chocolate";
+            }
+            case "T2" -> {
+                nombreProducto = "Turrón Clásico";
+            }
+        }
 
-                switch (code) {//Dependiendo del código guardamos el nombre del producto en una variable para mostrarlo luego
-                    case "M1" -> {
-                        nombreProducto = "Mantecados de Limón";
-                        precioUnidad = PRECIO_UNIDAD_M1_M2_P1;
-                    }
-                    case "M2" -> {
-                        nombreProducto = "Mazapán";
-                        precioUnidad = PRECIO_UNIDAD_M1_M2_P1;
-                    }
-                    case "P1" -> {
-                        nombreProducto = "Polvorón";
-                        precioUnidad = PRECIO_UNIDAD_M1_M2_P1;
-                    }
-                    case "T1" -> {
-                        nombreProducto = "Turrón de chocolate";
-                        precioUnidad = PRECIO_UNIDAD_T1_T2;
-                    }
-                    case "T2" -> {
-                        nombreProducto = "Turrón Clásico";
-                        precioUnidad = PRECIO_UNIDAD_T1_T2;
-                    }
-                }
-                //Dependiendo del código guardamos el nombre del producto en una variable para mostrarlo luego
-
-                //Para el cálculo hacemos un Math.ceil porque no podemos vender medias unidades y guardamos el resultado en un int
-                //Ya que al hacer Math.ceil nos quedará un número de decimales iguales a 0 el cual no perderá información
-                //al guardarlo en un int
-                int cantidadVenta = (int) Math.ceil((BENEFICIO_OBJETIVO / (precioUnidad - costeProduccionUnidad)));
-
-                //Guardamos el mensaje que queremos mostrar en ventana dentro de un textblock sin olvidar que debemos mostrar 2 decimales
-                String resultado = """
+        int cantidadVentaNecesaria = calculoVentaNecesariaObjetivo(precioVentaUnidad, costeProduccionUnidad);
+        //Guardamos el mensaje que queremos mostrar en ventana dentro de un textblock sin olvidar que debemos mostrar 2 decimales
+        String resultado = """
                             Referente al producto "%s" sabemos:
                             El coste de la materia prima es de %.2f €.
                             El precio de la mano de obra es de %.2f €.
@@ -139,12 +127,25 @@ public class EjercicioFabricaDulcesV2 {
                             
                             Y tendríamos que vender %d unidades para tener un beneficio de 2500€.
                             """.formatted(nombreProducto, costeMateriaPrimaUnidad, costeManoObra,
-                        costeProduccionUnidad, precioUnidad, cantidadVenta);
+                costeProduccionUnidad, precioVentaUnidad, cantidadVentaNecesaria);
 
-                JOptionPane.showMessageDialog(null, resultado);
-                break;
+        JOptionPane.showMessageDialog(null, resultado);
+    }
 
+    private static double calculoPrecioProduccion(double materiaPrima, double manoObra) {
+        return materiaPrima + manoObra;
+    }
+
+    private static double calculoPrecioUnidad(String code, double coste) {
+        if (code.equalsIgnoreCase("M1") || code.equalsIgnoreCase("M2") || code.equalsIgnoreCase("P1")) {
+            return (coste + (coste / 2));
+        } else {
+            return (coste + (coste * 65) / 100);
         }
+    }
+
+    private static int calculoVentaNecesariaObjetivo(double precioVenta, double costeProduccion) {
+        return ((int) Math.ceil((BENEFICIO_OBJETIVO / (precioVenta - costeProduccion))));
     }
 
 }
