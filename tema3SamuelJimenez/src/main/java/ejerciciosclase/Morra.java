97517fe08f7a29b9545eb0475dcfa1a22ae4f549
SamueljmDevs <samueljmdevs@gmail.com>
2022-11-03 15:39:23 +0100

    Añadidos comentarios

--- tema3SamuelJimenez/src/main/java/ejerciciosclase/Morra.java
+++ tema3SamuelJimenez/src/main/java/ejerciciosclase/Morra.java
@@ -4,7 +4,6 @@
  */
 package ejerciciosclase;
 
-import java.util.Random;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,28 +12,27 @@ import javax.swing.JOptionPane;
  */
 public class Morra {
 
-    /*
-    Programa para jugar a Morra. (Está en wikipedia)
-     */
-    private static Random rd = new Random();
-
     public static void main(String[] args) {
+        //Creamos las variables para guardar los datos necesarios y para finalizar el juego cuando corresponda
         int apuestaJugador, apuestaMaquina, totalDedos;
         int victoriasJugador = 0, victoriasMaquina = 0, numRondas = 0, rondasEmpate = 0, rondasNulas = 0;
         boolean finJuego = true;
 
-        do {
+        do {//Bucle del programa completo
             finJuego = false;
+            //Introducimos cuantos dedos queremos sacar, el método controla las excepciones y el rango permitido
             int numDedosJugador = utilidadespropias.Utilidades.intVentanaRangoIncluyente("¿Cuántos dedos desea sacar?", 5, 1);
 
-            int numDedosMaquina = dedosMaquina();
+            int numDedosMaquina = dedosMaquina();//LLamamos al método que hace sacar dedos a la máquina
 
             totalDedos = numDedosJugador + numDedosMaquina;
 
+            //Introducimos nuestra predicción
             apuestaJugador = utilidadespropias.Utilidades.intVentanaRangoIncluyente("¿Cuántos dedos crees que habrá en total?", 10, 2);
-
+            //Llamamos al método que dará la predicción de la máquina.
             apuestaMaquina = prediccionMaquina(numDedosMaquina);
 
+            //Mensaje para informar del resultado de la ronda
             String resumenRonda = """
                                En esta ronda...
                                El jugador sacó %d dedos.
@@ -51,32 +49,35 @@ public class Morra {
                                """.formatted(numDedosJugador, numDedosMaquina, totalDedos, apuestaJugador, apuestaMaquina);
 
             JOptionPane.showMessageDialog(null, resumenRonda);
-            if (apuestaJugador == totalDedos && apuestaMaquina == totalDedos) {
+            if (apuestaJugador == totalDedos && apuestaMaquina == totalDedos) {//En caso de que máquina y jugador acierten, se suma uno a los empates
                 JOptionPane.showMessageDialog(null, "Pues nada, empate.");
                 rondasEmpate++;
-            } else if (apuestaMaquina == totalDedos) {
+            } else if (apuestaMaquina == totalDedos) {//Si acierta la máquina se suma una victoria a su contador
                 JOptionPane.showMessageDialog(null, "Buuu... gana la máquina esta ronda...");
                 victoriasMaquina++;
-            } else if (apuestaJugador == totalDedos) {
+            } else if (apuestaJugador == totalDedos) {//Si acierta el jugador se suma uno a las victorias del jugador
                 JOptionPane.showMessageDialog(null, "Esta Ronda la gana el jugador!!!");
                 victoriasJugador++;
-            } else {
+            } else {//Si nadie acierta se suma uno al contador de rondas nulas
                 JOptionPane.showMessageDialog(null, "Nadie gana esta ronda");
                 rondasNulas++;
             }
-            numRondas++;
-            String contador = """
+            numRondas++;//Cada vuelta se suma uno al contador de rondas
+
+            //Mensaje para mostar el marcador
+            String marcador = """
                               Vamos por la ronda %d.
                               El jugador ha ganado %d rondas.
                               La máquina ha ganado %d rondas.
                               Ha habido %d rondas que fueron empate.
                               Ha habido %d rondas en las que nadie ganó.
                               """.formatted(numRondas, victoriasJugador, victoriasMaquina, rondasEmpate, rondasNulas);
+            //Mostramos el mensaje
+            JOptionPane.showMessageDialog(null, marcador);
 
-            JOptionPane.showMessageDialog(null, contador);
-
+            //Si el jugador lleva al menos 5 victoria y dos más que la máquina, gana el jugador
             if (victoriasJugador >= 5 && victoriasJugador >= victoriasMaquina + 2) {
-                finJuego = true;
+                finJuego = true;//Actualización de variable para finalizar el juego
                 String victoriaJugador = """
                                          ¡¡¡GANA EL JUGADOR!!!
                                          El jugador ha ganado la partida con:
@@ -86,8 +87,9 @@ public class Morra {
                                          """.formatted(victoriasJugador, victoriasMaquina, rondasEmpate);
 
                 JOptionPane.showMessageDialog(null, victoriaJugador);
+                //Si la máquina lleva al menos 5 victoria y dos más que el jugador, gana la máquina
             } else if (victoriasMaquina >= 5 && victoriasMaquina >= victoriasJugador + 2) {
-                finJuego = true;
+                finJuego = true;//Actualización de variable para finalizar el juego
                 String victoriaMaquina = """
                                          Pues... meh... ganó la máquina
                                          La máquina ha ganado la partida con:
@@ -98,26 +100,26 @@ public class Morra {
                 JOptionPane.showMessageDialog(null, victoriaMaquina);
             }
 
-            if (numRondas == 21) {
+            if (numRondas == 21) {//En el probable caso de que se alcancen las 21 rondas, gana quien más victorias lleve
                 if (victoriasJugador > victoriasMaquina) {
                     JOptionPane.showMessageDialog(null, "Gana el jugador con una diferencia de " + victoriasJugador + " a " + victoriasMaquina);
-                    finJuego = true;
+                    finJuego = true;//Actualización de variable para finalizar el juego
                 } else {
                     JOptionPane.showMessageDialog(null, "Gana la máquina con una diferencia de " + victoriasMaquina + " a " + victoriasJugador);
-                    finJuego = true;
+                    finJuego = true;//Actualización de variable para finalizar el juego
                 }
             }
         } while (!finJuego);
 
     }
 
-    private static int dedosMaquina() {
+    private static int dedosMaquina() {//Método que devuelve cuantos dedos saca la máquina
         int dedos = utilidadespropias.Utilidades.intRangoRandom(5, 1);
         return dedos;
     }
 
-    private static int prediccionMaquina(int dedos) {
-        int prediccion = utilidadespropias.Utilidades.intRangoRandom(dedos + 5, dedos);
+    private static int prediccionMaquina(int dedos) {//Método que devuelve la apuesta de la máquina
+        int prediccion = utilidadespropias.Utilidades.intRangoRandom(dedos + 5, dedos + 1);
         return prediccion;
     }
 
--- tema3SamuelJimenez/src/main/java/ejerciciosclase/MorraAproximacion.java
+++ tema3SamuelJimenez/src/main/java/ejerciciosclase/MorraAproximacion.java
@@ -13,32 +13,31 @@ import javax.swing.JOptionPane;
  */
 public class MorraAproximacion {
 
-    /**
-     * @param args the command line arguments
-     */
-    private static Random rd = new Random();
-
     public static void main(String[] args) {
+        //Creamos las variables para guardar los datos necesarios y para finalizar el juego cuando corresponda
         int apuestaJugador, apuestaMaquina, totalDedos, diferenciaJugador, diferenciaMaquina;
         int victoriasJugador = 0, victoriasMaquina = 0, numRondas = 0, rondasEmpate = 0;
         boolean finJuego = true;
 
-        do {
+        do {//Bucle del código completo
             finJuego = false;
+            //Introducimos cuantos dedos queremos sacar, el método controla las excepciones y el rango permitido
             int numDedosJugador = utilidadespropias.Utilidades.intVentanaRangoIncluyente("¿Cuántos dedos desea sacar?", 5, 1);
 
-            int numDedosMaquina = dedosMaquina();
+            int numDedosMaquina = dedosMaquina();//LLamamos al método que hace sacar dedos a la máquina
 
             totalDedos = numDedosJugador + numDedosMaquina;
 
+            //Introducimos nuestra predicción
             apuestaJugador = utilidadespropias.Utilidades.intVentanaRangoIncluyente("¿Cuántos dedos crees que habrá en total?", 10, 2);
-
+            //Llamamos al método que dará la predicción de la máquina.
             apuestaMaquina = prediccionMaquina(numDedosMaquina);
 
+            //Calculamos la diferencia entre el resultado real y nuestra predicción
             diferenciaJugador = Math.abs(totalDedos - apuestaJugador);
-
             diferenciaMaquina = Math.abs(totalDedos - apuestaMaquina);
 
+            //Mensaje para informar del resultado de la ronda
             String resumenRonda = """
                                En esta ronda...
                                El jugador sacó %d dedos.
@@ -53,31 +52,35 @@ public class MorraAproximacion {
                                
                                Por lo tanto...
                                """.formatted(numDedosJugador, numDedosMaquina, totalDedos, apuestaJugador, apuestaMaquina);
-
+            //Mostramos el mensaje
             JOptionPane.showMessageDialog(null, resumenRonda);
-            if (apuestaJugador == totalDedos && apuestaMaquina == totalDedos) {
+
+            if (apuestaJugador == totalDedos && apuestaMaquina == totalDedos) {//Si jugador y máquina acertaron se suma uno a las rondas empatadas
                 JOptionPane.showMessageDialog(null, "Pues nada, empate.");
                 rondasEmpate++;
-            } else if (diferenciaJugador > diferenciaMaquina) {
+            } else if (diferenciaJugador > diferenciaMaquina) {//Si le diferencia de la máquina es menor a la del jugador se le suma una victoria
                 JOptionPane.showMessageDialog(null, "Buuu... gana la máquina esta ronda...");
                 victoriasMaquina++;
-            } else {
+            } else {//En caso contrario al caso anterior se suma una victoria al jugador
                 JOptionPane.showMessageDialog(null, "Esta Ronda la gana el jugador!!!");
                 victoriasJugador++;
 
             }
+            //Sumamos uno al contador de rondas
             numRondas++;
-            String contador = """
+            //Mensaje para ver como va el marcador
+            String marcador = """
                               Vamos por la ronda %d.
                               El jugador ha ganado %d rondas.
                               La máquina ha ganado %d rondas.
                               Ha habido %d rondas que fueron empate.
                               """.formatted(numRondas, victoriasJugador, victoriasMaquina, rondasEmpate);
+            //Mostramos el marcador
+            JOptionPane.showMessageDialog(null, marcador);
 
-            JOptionPane.showMessageDialog(null, contador);
-
+            //Si el jugador ha ganado al menos 5 rondas y lleva al menos dos victorias más que la máquina, el jugador gana
             if (victoriasJugador >= 5 && victoriasJugador >= victoriasMaquina + 2) {
-                finJuego = true;
+                finJuego = true;//Actualización de variable para finalizar el juego
                 String victoriaJugador = """
                                          ¡¡¡GANA EL JUGADOR!!!
                                          El jugador ha ganado la partida con:
@@ -87,8 +90,9 @@ public class MorraAproximacion {
                                          """.formatted(victoriasJugador, victoriasMaquina, rondasEmpate);
 
                 JOptionPane.showMessageDialog(null, victoriaJugador);
+                //Si la máquina es quien lleva 5 o más victorias y 2 o más que el jugador, gana la máquina
             } else if (victoriasMaquina >= 5 && victoriasMaquina >= victoriasJugador + 2) {
-                finJuego = true;
+                finJuego = true;//Actualización de variable para finalizar el juego
                 String victoriaMaquina = """
                                          Pues... meh... ganó la máquina
                                          La máquina ha ganado la partida con:
@@ -98,27 +102,27 @@ public class MorraAproximacion {
                                          """.formatted(victoriasMaquina, victoriasJugador, rondasEmpate);
                 JOptionPane.showMessageDialog(null, victoriaMaquina);
             }
-
+            //En el improbable caso de que nadie gane en la ronda 21, ganará quien lleve más victorias, independientemente de la diferencia
             if (numRondas == 21) {
                 if (victoriasJugador > victoriasMaquina) {
                     JOptionPane.showMessageDialog(null, "Gana el jugador con una diferencia de " + victoriasJugador + " a " + victoriasMaquina);
-                    finJuego = true;
+                    finJuego = true;//Actualización de variable para finalizar el juego
                 } else {
                     JOptionPane.showMessageDialog(null, "Gana la máquina con una diferencia de " + victoriasMaquina + " a " + victoriasJugador);
-                    finJuego = true;
+                    finJuego = true;//Actualización de variable para finalizar el juego
                 }
             }
         } while (!finJuego);
 
     }
 
-    private static int dedosMaquina() {
+    private static int dedosMaquina() {//Método que devuelve los dedos que saca la máquina.
         int dedos = utilidadespropias.Utilidades.intRangoRandom(5, 1);
         return dedos;
     }
 
-    private static int prediccionMaquina(int dedos) {
-        int prediccion = utilidadespropias.Utilidades.intRangoRandom(dedos + 5, dedos);
+    private static int prediccionMaquina(int dedos) {//Método que devuelve la apuesta de la máquina
+        int prediccion = utilidadespropias.Utilidades.intRangoRandom(dedos + 5, dedos + 1);
         return prediccion;
     }
 
