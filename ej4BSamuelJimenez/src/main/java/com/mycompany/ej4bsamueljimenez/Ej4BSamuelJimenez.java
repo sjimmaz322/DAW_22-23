bf6e324951b18bceeade072185a42500716b6e2c
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-11-07 14:00:57 +0100

    Creado ejercicio 4B

---
+++ ej4BSamuelJimenez/src/main/java/com/mycompany/ej4bsamueljimenez/Ej4BSamuelJimenez.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ej4bsamueljimenez;
+
+/**
+ *
+ * @author samuel
+ */
+public class Ej4BSamuelJimenez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
