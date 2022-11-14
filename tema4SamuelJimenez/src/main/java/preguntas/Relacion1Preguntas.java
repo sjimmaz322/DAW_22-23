0502d486f1c6860604d379c2794095a1cbdad050
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-11-14 13:06:18 +0100

    Creada clase con las preguntas de la primera relación de preguntas de P.O.O.

---
+++ tema4SamuelJimenez/src/main/java/preguntas/Relacion1Preguntas.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package preguntas;
+
+/**
+ *
+ * @author samuel
+ */
+public class Relacion1Preguntas {
+    /*
+¿Qué es un programa orientado a objetos?
+    
+¿Qué es una clase?
+    
+¿Qué es un objeto?
+    
+¿Cuáles son los pilares de la POO? Explica brevemente cada uno de ellos.
+    
+¿Qué ventajas aporta la POO?
+    
+¿Cuáles son las partes de una clase?
+    
+¿Qué elementos componen la definición de una clase? Explica brevemente cada uno
+    
+¿Qué es un método constructor?
+    
+¿Qué son los métodos de consulta o getters?
+    
+¿Qué son los métodos modificadores o setters?
+    
+¿En qué consiste la instanciación de un objeto? ¿Para qué sirve el operador ‘new’?
+    
+¿Cuál es la función del método main()? ¿Cómo se declara el método main()?
+    
+¿Cómo interaccionan los objetos de una aplicación?
+
+     */
+
+}
