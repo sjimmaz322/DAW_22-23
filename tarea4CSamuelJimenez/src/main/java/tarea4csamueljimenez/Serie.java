08b72cf7e6cf5ad823d22542b57a0813f4dea6d8
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-11-16 14:43:25 +0100

    Añadida clase serie

---
+++ tarea4CSamuelJimenez/src/main/java/tarea4csamueljimenez/Serie.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea4csamueljimenez;
+
+/**
+ *
+ * @author samuel
+ */
+public class Serie {
+
+    /*
+    Serie: Título, genero (Acción, comedia, Scifi,Drama),sinopsis,  número de temporadas, director/productor.
+     */
+    private String titulo, genero, sinopsis, directorProductor;
+    private int numTemporadas, numLikes;
+
+    public Serie() {
+        this.titulo = "";
+        this.genero = "";
+        this.sinopsis = "";
+        this.directorProductor = "";
+        this.numTemporadas = 0;
+        this.numLikes = 0;
+    }
+
+    public Serie(String titulo, String genero, String sinopsis, String directorProductor, int numTemporadas) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.sinopsis = sinopsis;
+        this.directorProductor = directorProductor;
+        this.numTemporadas = numTemporadas;
+    }
+
+    public Serie(String titulo, String genero, String sinopsis, String directorProductor, int numTemporadas, int numLikes) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.sinopsis = sinopsis;
+        this.directorProductor = directorProductor;
+        this.numTemporadas = numTemporadas;
+        this.numLikes = numLikes;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getDirectorProductor() {
+        return directorProductor;
+    }
+
+    public void setDirectorProductor(String directorProductor) {
+        this.directorProductor = directorProductor;
+    }
+
+    public int getNumTemporadas() {
+        return numTemporadas;
+    }
+
+    public void setNumTemporadas(int numTemporadas) {
+        this.numTemporadas = numTemporadas;
+    }
+
+    public int getNumLikes() {
+        return numLikes;
+    }
+
+    public void setNumLikes(int numLikes) {
+        this.numLikes = numLikes;
+    }
+
+    public void darLike() {
+        this.numLikes++;
+    }
+
+    @Override
+    public String toString() {
+        return "La serie " + titulo + "\n"
+                + "Es del género " + genero + "\n"
+                + "Dirigida/Producida por " + directorProductor + "\n"
+                + "Con " + numTemporadas + " temporadas \n"
+                + "Y cuya sinopsis es \n<<" + sinopsis + ">>\n"
+                + "Y tiene un número de likes de " + numLikes;
+
+    }
+
+}
