3d753e3a44e0f6a7647dc0d99a69f4758435e344
sjimmaz322 <sjmmaz322@g.educaand.es>
2022-11-23 14:11:36 +0100

    Terminado ejercicio

--- ejercicio4DSamuelJimenez/src/main/java/daw/programa/Prueba.java
+++ ejercicio4DSamuelJimenez/src/main/java/daw/programa/Prueba.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package animales;
+package daw.programa;
 
+import daw.samueljimenez.Animal;
+import daw.samueljimenez.Persona;
 import java.time.LocalDate;
 
 /**
