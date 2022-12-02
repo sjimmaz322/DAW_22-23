bf259e1f1b22b3ee9979d997848f91003a615b4d
sjimmaz322 <sjmmaz322@g.educaand.es>
2022-12-02 10:33:18 +0100

    Corregido ejercicio

--- tema5SamuelJimenez/src/main/java/ejercicios/Ej5.java
+++ tema5SamuelJimenez/src/main/java/ejercicios/Ej5.java
@@ -15,33 +15,106 @@ public class Ej5 {
     /**
      * @param args the command line arguments
      */
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         int[] nums = new int[10];
-        int contadorPos = 0, contadorNeg = 0, contadorCeros = 0;
+        int numPos = 0, numNeg = 0, numCeros = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println("Introduza un número entero");
-            nums[i] = sc.nextInt();
-            if (nums[i] == 0) {
-                contadorCeros++;
-            } else if (nums[i] > 0) {
-                contadorPos++;
-            } else {
-                contadorNeg++;
-            }
-        }
+        rellenarAray(nums);
 
-        String mensaje = """
-                         En el Array, de los 10 números hay:
-                         %d números positivos.
-                         %d números negativos.
-                         %d ceros.
-                         """.formatted(contadorPos, contadorNeg, contadorCeros);
+        numPos = contarPositivos(nums);
+        numNeg = contarNegativos(nums);
+        numCeros = contarCeros(nums);
 
-        System.out.println(mensaje);
+        mostrarArray(nums);
 
+        mediaPositvos(nums);
+        mediaNegativos(nums);
+
+//        String mensaje = """
+//                         En el Array, de los 10 números hay:
+//                         %d números positivos.
+//                         %d números negativos.
+//                         %d ceros.
+//                         """.formatted(numPos, numNeg, numCeros);
+//
+//        System.out.println(mensaje);
     }
 
+    public static void rellenarAray(int[] aux) {
+        for (int i = 0; i < aux.length; i++) {
+            System.out.println("Introduza un número entero");
+            aux[i] = sc.nextInt();
+        }
+    }
+
+    private static int contarPositivos(int[] aux) {
+        int pos = 0;
+        for (int i = 0; i < aux.length; i++) {
+            if (aux[i] > 0) {
+                pos++;
+            }
+        }
+        return pos;
+    }
+
+    private static int contarNegativos(int[] aux) {
+        int neg = 0;
+        for (int i = 0; i < aux.length; i++) {
+            if (aux[i] < 0) {
+                neg++;
+            }
+        }
+        return neg;
+    }
+
+    private static int contarCeros(int[] aux) {
+        int ceros = 0;
+        for (int i = 0; i < aux.length; i++) {
+            if (aux[i] == 0) {
+                ceros++;
+            }
+        }
+        return ceros;
+    }
+
+    private static void mostrarArray(int[] aux) {
+        for (int i = 0; i < aux.length; i++) {
+            System.out.println("El valor de la posición " + (i + 1) + " es " + aux[i]);
+        }
+    }
+
+    private static void mediaPositvos(int[] aux) {
+        int media = 0, suma = 0;
+
+        for (int i = 0; i < aux.length; i++) {
+            if (aux[i] > 0) {
+                suma += aux[i];
+            }
+        }
+        try{
+        media = suma / contarPositivos(aux);
+        }catch(ArithmeticException ae){
+            System.out.println("No has puesto ningún número positivo");
+        }
+        System.out.println("La media de los números positivos es " + media);
+    }
+
+    private static void mediaNegativos(int[] aux) {
+        int media = 0, suma = 0;
+
+        for (int i = 0; i < aux.length; i++) {
+            if (aux[i] < 0) {
+                suma += aux[i];
+            }
+        }
+         try{
+        media = suma / contarNegativos(aux);
+        }catch(ArithmeticException ae){
+            System.out.println("No has puesto ningún número negativo");
+        }
+        System.out.println("La media de los números negativos es " + media);
+    }
 }
