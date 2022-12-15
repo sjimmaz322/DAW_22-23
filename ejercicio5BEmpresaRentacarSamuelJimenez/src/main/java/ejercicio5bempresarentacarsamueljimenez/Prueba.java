682b749cc6d82316077a8b1bc74a1d11b80ef319
sjimmaz322 <sjmmaz322@g.educaand.es>
2022-12-15 11:12:05 +0100

    Añadido RandomUtils y clase para pruebas

---
+++ ejercicio5BEmpresaRentacarSamuelJimenez/src/main/java/ejercicio5bempresarentacarsamueljimenez/Prueba.java
@@ -0,0 +1,17 @@
+package ejercicio5bempresarentacarsamueljimenez;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+/**
+ *
+ * @author samuel
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        Empresa rentacar = new Empresa();
+        System.out.println(rentacar);
+    }
+}
