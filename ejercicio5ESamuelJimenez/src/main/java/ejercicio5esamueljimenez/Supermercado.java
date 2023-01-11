9153647c02ba8d41f8ecd947f72c08e43bfb7e55
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-01-11 13:04:14 +0100

    Miau

--- ejercicio5ESamuelJimenez/src/main/java/ejercicio5esamueljimenez/Supermercado.java
+++ ejercicio5ESamuelJimenez/src/main/java/ejercicio5esamueljimenez/Supermercado.java
@@ -8,29 +8,37 @@ package ejercicio5esamueljimenez;
  *
  * @author samuel
  */
-public class Prueba {
+public class Supermercado {
 
     /**
      * @param args the command line arguments
      */
+    private static Ticket generarTicket(Caja c) {
+        Ticket t = c.generarTicket();
+        return t;
+    }
+
     public static void main(String[] args) {
-        
+        //-----
         Producto p1 = new Producto("Patata", 2, 4, 4);
         Producto p2 = new Producto("Gel Baño", 1, 5.15, 10);
         Producto p3 = new Producto("Queso", 4, 9, 21);
         Producto p4 = new Producto("Colonia", 1, 2.99, 10);
-        
+        //-----
         Caja c1 = new Caja();
+        //-----
         c1.aniadirProducto(p1);
         c1.aniadirProducto(p2);
         c1.aniadirProducto(p3);
         c1.aniadirProducto(p4);
-        
-        Ticket t = c1.generarTicket();
+        //-----
+        Ticket t = generarTicket(c1);
+        //-----
         t.setNombreSuper("PacheMás");
-        
+        //-----
         System.out.println(t);
+        //-----
 
     }
-    
+
 }
