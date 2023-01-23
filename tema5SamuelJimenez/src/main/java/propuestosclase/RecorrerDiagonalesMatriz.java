aea96b0c642c693587c0948df8410874b1004987
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-01-23 13:27:57 +0100

    Creadas clases, en el comentario explico lo que piden

---
+++ tema5SamuelJimenez/src/main/java/propuestosclase/RecorrerDiagonalesMatriz.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package propuestosclase;
+
+/**
+ *
+ * @author samuel
+ */
+public class RecorrerDiagonalesMatriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Recorrer diagonales de una matriz cuadrada y devolvemos los valores que toquen.
+        Diagonal principal   \    y diagonal secundaria     /
+        */
+    }
+    
+}
---
+++ tema5SamuelJimenez/src/main/java/propuestosclase/RecorrerVecinasMatriz.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package propuestosclase;
+
+/**
+ *
+ * @author samuel
+ */
+public class RecorrerVecinasMatriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Mostrar las vecinas de una coordenada de una matriz cuadrada
+        */
+    }
+    
+}
