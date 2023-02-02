b47439e9b129b83a04a3568d9eb313ec5e66b386
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-02-02 12:44:54 +0100

    Eliminado enunciado

--- tema5SamuelJimenez/src/main/java/ejerciciopilagenerico/Pila.java
+++ tema5SamuelJimenez/src/main/java/ejerciciopilagenerico/Pila.java
@@ -13,24 +13,6 @@ import java.util.Arrays;
  */
 public class Pila<T> {
 
-    /*
-    Implementa una Pila (Stack, en inglés) usando clases genéricas. Una pila es un almacén de datos en el que se sigue una política específica a la hora de introducir y sacar los elementos.
-    Una pila sigue una política LIFO (Last In First Out), es decir, el último elemento en entrar (apilar) es el primero en salir (desapilar). 
-    La pila tiene un tamaño máximo, de forma que no se pueden guardar más elementos que los indicados en ese tamaño. 
-    Gráficamente, una pila de números se puede representar según la siguiente imagen:
-
-
-    En este ejercicio se pide implementar una clase que modele el comportamiento de una pila, para cualquier tipo de objetos, mediante un ArrayList, ofreciendo la siguiente funcionalidad:
-    La pila siempre se creará vacía. X
-    apilar elemento (en inglés push), lo inserta donde proceda. X
-    desapilar (en inglés pop), que saca el elemento que toque. X
-    Saber si la pila está vacía. X
-    Saber si la pila está llena. X
-    Saber el número de elementos que contiene la pila. X
-    Mostrar por consola los elementos de la pila, según el orden en el que saldrían los elementos de la pila. Esta operación no debe alterar la estructura de datos.
-    rellenar (T[] array), almacena en la pila los objetos que hay en el array. La pila se ajusta su tamaño según el número de elementos que tenga el array.
-    T[] sacarElementos(), saca los elementos de la pila y los devuelve en un array de T. La pila, después de llamar a este método, queda vacía.  
-     */
     private ArrayList<T> pila;
 
     public Pila() {
@@ -68,7 +50,7 @@ public class Pila<T> {
     public void mostrarLista() {
         int pos = this.pila.size();
         for (int i = pila.size() - 1; i >= 0; i--) {
-            System.out.println("Elemento "+pos+" - "+this.pila.get(i));
+            System.out.println("Elemento " + pos + " - " + this.pila.get(i));
             pos--;
         }
     }
