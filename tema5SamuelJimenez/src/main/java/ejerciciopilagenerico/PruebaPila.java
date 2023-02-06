5d128a3d9a522fe450bb034065d11b65d5db9b97
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-02-06 12:53:16 +0100

    Añadidas últimas pruebas de métodos

--- tema5SamuelJimenez/src/main/java/ejerciciopilagenerico/PruebaPila.java
+++ tema5SamuelJimenez/src/main/java/ejerciciopilagenerico/PruebaPila.java
@@ -19,7 +19,7 @@ public class PruebaPila {
         Pila<Cancion> pilaCan = new Pila<>();
         //---
         Cancion c1 = new Cancion("Muerte a pellizcos", "Vico", 2022);
-        Cancion c2 = new Cancion("Ser un manteca", "Vico", 2022);
+        Cancion c2 = new Cancion("Eres un manteca", "Vico", 2022);
         Cancion c3 = new Cancion("Bueno, ¿qué me contáis?", "Vico", 2022);
         //---
         pilaCan.push(c1);
@@ -34,6 +34,25 @@ public class PruebaPila {
         pilaStr.push("Princeso");
         //---
         pilaStr.mostrarLista();
+        //--
+        String estado = pilaCan.pilaLlena();
+        System.out.println(estado);
+        //--
+        estado = pilaCan.pilaVacia();
+        System.out.println("\n" + estado);
+        //--
+        Object[] listaCanciones = pilaCan.sacarElementos();
+        for (int i = 0; i < listaCanciones.length; i++) {
+            System.out.println(listaCanciones[i]);
+
+        }
+        //--
+        estado = pilaCan.pilaLlena();
+        System.out.println(estado);
+        //--
+        estado = pilaCan.pilaVacia();
+        System.out.println("\n" + estado);
+        //--
     }
 
 }
