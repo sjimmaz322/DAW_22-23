9e8d5a94193564768cba0736a11f947dd5d04e5e
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-02-06 14:36:51 +0100

    Corregidos fallos y cread factorial con recursividad

--- tema5SamuelJimenez/src/main/java/recursividad/EjemploRecursividad.java
+++ tema5SamuelJimenez/src/main/java/recursividad/EjemploRecursividad.java
@@ -14,7 +14,44 @@ public class EjemploRecursividad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //---
+        recursion(5);
+        //---
+        System.out.println("Imprimimos el factorial iterativo");
+        factorial(5);
+        //---
+        System.out.println("Imprimimos el factorial recursivo");
+        int numero = factorialRecursivo(5);
+        System.out.println(numero);
+        //---
+    }
+
+    public static void recursion(int n) {
+        if (n > 0) {//--- Caso Base
+            recursion(n - 1);//--- Ciclo activo
+        }
+
+        System.out.println(n);
+    }
+
+    public static void factorial(int n) {
+        int num = 0;
+        for (int i = n; i >= 0; i--) {
+            num += n * (n - 1);
+        }
+        System.out.println(num);
+
+    }
+
+    public static int factorialRecursivo(int n) {
+
+        n = Math.abs(n);
+
+        if (n == 0 || n == 1) {//--- Caso base
+            return 1;
+        } else {
+            return n * factorialRecursivo(n - 1); //--- Ciclo activo
+        }
     }
 
 }
