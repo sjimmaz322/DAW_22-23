a7f4ffb63412139c5e59fc9190d76947f1a02f6c
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-02-06 14:42:38 +0100

    Añadidos objetos de subclases de vehiculo

--- tema6SamuelJimenez/src/main/java/vehiculos/PruebaVehiculosç.java
+++ tema6SamuelJimenez/src/main/java/vehiculos/PruebaVehiculosç.java
@@ -15,6 +15,11 @@ public class PruebaVehiculosç {
      */
     public static void main(String[] args) {
 
+        Turismo t1 = new Turismo(987456321L, "1744-KHT", "Dacia", "Sandero", "Blanco", 105, 5, true);
+        Furgoneta f1 = new Furgoneta(154789654L, "1324-ABC", "Renault", "Kangoo", "Verde", 120, 3400, 3000);
+        Deportivo d1 = new Deportivo(123654789L, "6582-RCC", "Ferrari", "Testarossa", "Rojo", 200, 1500);
+        //---
+
     }
 
 }
