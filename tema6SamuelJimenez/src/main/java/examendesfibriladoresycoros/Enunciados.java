c9e0724826dd753ec24b6a5a78f4284c9b67ec8b
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-03-15 14:24:35 +0100

    Nada importante

--- tema6SamuelJimenez/src/main/java/examendesfibriladoresycoros/Enunciados.java
+++ tema6SamuelJimenez/src/main/java/examendesfibriladoresycoros/Enunciados.java
@@ -30,16 +30,6 @@ public class Enunciados {
     (0,75 puntos) Obtener el número de desfibriladores asociados a un correo electrónico. Devolverá una nueva estructura Map con la información anterior.
     Crea la clase Prueba (0,25 puntos), en ej2, para crear un objeto MapDesfibrilador y probar los métodos anteriores.
     
-    EJ3 (5 puntos).- En un nuevo paquete llamado ej3, crea un Enum llamado Voz con seis elementos: CONTRATENOR, TENOR, BARITONO, SOPRANO, MEZZOSOPRANO y CONTRALTO. 
-    Las tres primeras son voces masculinas y las tres últimas son femeninas. Consideraremos un coro de voces mixtas aquel en el que hay al menos un tipo de voz diferente, de las anteriores. 
-    Crea una clase, llamada Coro, sin atributos de instancia que incluya los siguientes métodos de clase públicos. Si existen atributos de clase serán privados. 
-    Si existen otros métodos, serán privados. Se pueden crear otras clases, si se estima necesario.
-    (0,75 puntos) generarVoces, que recibe un número entero mayor o igual a 6, y debe devolver un objeto tipo List<Voz> con tantas voces (masculinas y femeninas) aleatorias como indique el parámetro. 
-    En caso de que el parámetro sea incorrecto, se lanzará una excepción de tipo IllegalArgument.
-    (2 puntos) getNumeroCorosMixtos, que recibe la lista generada por el método anterior y devuelve el número de coros mixtos que se pueden agrupar según las voces contenidas en la lista.
-    (2 puntos) buscarVozMasRepetida, que recibe la lista generada por el método implementado en A y devuelve un conjunto único de voces indicando las voces que más se repiten en la lista.
-    Por ejemplo, si en una lista de 20 voces hay dos voces (A y B) con 5 repeticiones cada una y son las que más se repiten, debería devolver esas dos voces (A y B).
-    En una clase llamada PruebaCoro (0,25 puntos), dentro de ej3, implementa el código necesario para generar una lista de 20 voces aleatorias, mostrar por pantalla el resultado y probar los métodos B y C.
-
+    
      */
 }
