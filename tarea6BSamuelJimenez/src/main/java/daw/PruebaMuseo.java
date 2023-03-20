27b015669c338a04c283431392ac4a8a5d942bf4
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-03-20 14:42:27 +0100

    Terminado diagrama UML

--- tarea6BSamuelJimenez/src/main/java/daw/PruebaMuseo.java
+++ tarea6BSamuelJimenez/src/main/java/daw/PruebaMuseo.java
@@ -2,10 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package museo;
+package daw;
 
-import java.time.LocalDate;
-import java.util.List;
+    
 
 /**
  *
@@ -17,19 +16,7 @@ public class PruebaMuseo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //---
-        List<Empleado> listaEmp = UtilidadesMuseo.getListaEmpleados();
-        List<Sala> listaSalas = UtilidadesMuseo.getListaSalas(new Humedad(60, 30, 1), new Temperatura(25, 15, 2));
-        //---
-        Museo m1 = new Museo("Primer Museo", "Calle Falsa 123", listaSalas, listaEmp);
-        //---
-        System.out.println(m1);
-        //---
-        System.out.println(m1.getSalasMuseo().size());
-        m1.quitarSala(m1.getSalasMuseo().get(1));
-        System.out.println(m1.getSalasMuseo().size());
-        m1.aniadirSala(new Temporal());
-        System.out.println(m1.getSalasMuseo().size());
+       
     }
 
 }
