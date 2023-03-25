ecdeae9a1880029854e66cab4ab12dacffa0269e
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-03-25 13:05:59 +0100

    Terminados ejercicios de expresiones regulares

--- tema6SamuelJimenez/src/main/java/exprregulares/EjRegex.java
+++ tema6SamuelJimenez/src/main/java/exprregulares/EjRegex.java
@@ -48,6 +48,35 @@ public class EjRegex {
             System.out.println("Patrón encontrado: " + matcher.group());
         }
 
+        /*----- EJERCICIO 2 -----*/
+        //---
+        String regex2 = "ini[0-9]{2}fin";
+        //---
+        String txt2 = "asogvihsuihgerhjtgbsevhgfini13finasjkfgyuegbhjaesbgyuhdgini87finajksfhbujewgbysdhuysdjt";
+        //---
+        Pattern pat2 = Pattern.compile(regex2);
+        //---
+        Matcher matcher2 = pat2.matcher(txt2);
+        //---
+        while (matcher2.find()) {
+
+            System.out.println("Patrón encontrado: " + matcher2.group());
+        }
+
+        /*----- EJERCICIO 3 -----*/
+        //---
+        String regex3 = "[a-z|A-Z]{1}[0-9]{1}";
+        //---
+        String txt3 = "a4deslkjigbyfdhddfshdf8hjn48f7hdf8hy487748FDH4NFDGCJN7FXD8HJRD8H8DRB7FC87H89D/RF7HJ98DRF7JH89D";
+        //---
+        Pattern pat3 = Pattern.compile(regex3);
+        //---
+        Matcher matcher3 = pat3.matcher(txt3);
+        //---
+         while (matcher3.find()) {
+
+            System.out.println("Patrón encontrado: " + matcher3.group());
+        }
     }
 
 }
