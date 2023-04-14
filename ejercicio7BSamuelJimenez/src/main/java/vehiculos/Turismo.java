b37a766e1c418dce9a0691268296e8611283d73c
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-04-14 11:13:54 +0200

    Copiadas clases necesarias

---
+++ ejercicio7BSamuelJimenez/src/main/java/vehiculos/Turismo.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vehiculos;
+
+/**
+ *
+ * @author samuel
+ */
+public class Turismo extends Vehiculo {
+
+    private int puertas;
+    private boolean marchaAutomatica;
+
+    public Turismo(Long bastidor, String matricula, String marca, String modelo, String color, double tarifa, int puertas, boolean marchaAutomatica) {
+        super(bastidor, matricula, marca, modelo, color, tarifa);
+        this.puertas = puertas;
+        this.marchaAutomatica = marchaAutomatica;
+    }
+
+    public Turismo() {
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
+
+    public boolean isMarchaAutomatica() {
+        return marchaAutomatica;
+    }
+
+    public void setMarchaAutomatica(boolean marchaAutomatica) {
+        this.marchaAutomatica = marchaAutomatica;
+    }
+
+    public void arrancar() {
+        System.out.println("El coche está encendido para irse con la familia");
+    }
+
+    public void apagar() {
+        System.out.println("Se terminó el viaje");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + puertas + ":" + marchaAutomatica + ":";
+    }
+
+}
---
+++ ejercicio7BSamuelJimenez/src/main/java/vehiculos/Vehiculo.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vehiculos;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author samuel
+ */
+public abstract class Vehiculo {
+
+    private Long bastidor;
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double tarifa;
+    private boolean disponible;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(Long bastidor, String matricula, String marca, String modelo, String color, double tarifa) {
+        this.bastidor = bastidor;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = false;
+    }
+
+    public Long getBastidor() {
+        return bastidor;
+    }
+
+    public void setBastidor(Long bastidor) {
+        this.bastidor = bastidor;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public abstract void arrancar();
+
+    public abstract void apagar();
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.bastidor);
+        hash = 23 * hash + Objects.hashCode(this.matricula);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehiculo other = (Vehiculo) obj;
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        return Objects.equals(this.bastidor, other.bastidor);
+    }
+
+    @Override
+    public String toString() {
+        return bastidor + ":" + matricula + ":" + marca + ":" + modelo + ":" + color + ":" + tarifa + ":" + disponible + ":";
+    }
+
+}
