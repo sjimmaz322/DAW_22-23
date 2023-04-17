3c375b5e663d31cc67d53c3437108fe9b8635354
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-04-17 12:34:40 +0200

    Miau

--- ejercicio7BSamuelJimenez/src/main/java/daw/Ejercicio7BSamuelJimenez.java
+++ ejercicio7BSamuelJimenez/src/main/java/daw/Ejercicio7BSamuelJimenez.java
@@ -255,7 +255,6 @@ public class Ejercicio7BSamuelJimenez {
                 //--- Usamos los tokens[] para guardar cada atributo según el constructor
                 if (linea.contains("0 - ")) {//--- Si es un turismo
                     tokens[0] = tokens[0].replaceAll("0 - ", "");
-
                     v = new Turismo(Long.valueOf(tokens[0]), tokens[1], tokens[2], tokens[3], tokens[4], Double.parseDouble(tokens[5]), Integer.parseInt(tokens[7]), Boolean.parseBoolean(tokens[8]));
                 }
                 if (linea.contains("1 - ")) {//--- Si es un deportivo
