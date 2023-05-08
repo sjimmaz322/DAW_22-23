381adb2a1cb2f55d382acc008c7434c55e94f52d
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-05-08 14:25:55 +0200

    Añadidas clases

---
+++ p81SamuelJimenez/src/main/java/p81samueljimenez/LocalDateAdapter.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p81samueljimenez;
+
+import java.time.LocalDate;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ *
+ * @author samuel
+ */
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+    @Override
+    public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v);
+    }
+
+    @Override
+    public String marshal(LocalDate v) throws Exception {
+        return v.toString();
+    }
+}
